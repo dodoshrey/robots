fdc3d0fcc30aaf0d5ecceb8777a8247572a263dc
Shrey Wahi <wahishrey@gmail.com>
2019-08-22 19:46:28 +0530

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,104 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'S.P. Wahi',
+    username: 'SP',
+    email: 'sp_wahi@wahi.biz'
+  },
+  {
+    id: 2,
+    name: 'S.K. Dhingra',
+    username: 'SK',
+    email: 'sk_dhingra@dhingra.tv'
+  },
+  {
+    id: 3,
+    name: 'Kanchan Wahi',
+    username: 'Kanchan',
+    email: 'kanchan@wahi.net'
+  },
+  {
+    id: 4,
+    name: 'Sneh Lata Dhingra',
+    username: 'Sneh',
+    email: 'sneh_lata@dhingra.org'
+  },
+  {
+    id: 5,
+    name: 'Vikas Wahi',
+    username: 'Babblu',
+    email: 'babblu_wahi@walking.man'
+  },
+  {
+    id: 6,
+    name: 'Vivek Wahi',
+    username: 'Tony',
+    email: 'tony_w@mail.info'
+  },
+  {
+    id: 7,
+    name: 'Sandeep Wahi',
+    username: 'Deepu',
+    email: 'sandy@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Dolly Wahi',
+    username: 'Dolly',
+    email: 'dolly@melmel.me'
+  },
+  {
+    id: 9,
+    name: 'Meenu Wahi',
+    username: 'Meenu',
+    email: 'meenu_w@dana.to'
+  },
+  {
+    id: 10,
+    name: 'Neeraj Dhingra',
+    username: 'Goldy',
+    email: 'goldy@mama.biz'
+  },
+  {
+    id: 11,
+    name: 'Neeru Dhingra',
+    username: 'Neeru',
+    email: 'neeru@chhhh.io'
+  },
+  {
+    id: 12,
+    name: 'Anmol Wahi',
+    username: 'Anmol',
+    email: 'anmol_22@email.to'
+  },
+  {
+    id: 13,
+    name: 'Surbhi Wahi',
+    username: 'Surbhi',
+    email: 'surbhi@mars.com'
+  },
+  {
+    id: 14,
+    name: 'Shrey Wahi',
+    username: 'Shrey',
+    email: 'shrey_wahi@mail.com'
+  },
+  {
+    id: 15,
+    name: 'Dodo Wahi',
+    username: 'Dodo',
+    email: 'dodo_cool@doggo.in'
+  },
+  {
+    id: 16,
+    name: 'Saniya Dhingra',
+    username: 'Saniya',
+    email: 'saniya@dhingra.co'
+  },
+  {
+    id: 17,
+    name: 'Divyansh Dhingra',
+    username: 'Divyansh',
+    email: 'divyansh_roxxx@dana.io'
+  }
+];
