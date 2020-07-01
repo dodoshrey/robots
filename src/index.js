983ef0b382a6ebed16ba5738a2d968fb2af5d94f
Shrey Wahi <51951179+dodoshrey@users.noreply.github.com>
2020-07-01 05:37:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import Card from './Card';
-//import CardList from './CardList';
 import App from './Containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
@@ -10,7 +8,4 @@ import 'tachyons';
 ReactDOM.render(
     <App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
